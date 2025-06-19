0357a6fb340e3ceadeb6dca3888d935562e30ddc
khushi <khushipurohit171@gmail.com>
2025-06-19 23:05:40 +0530

    new added

---
+++ src/leetcode/good_array.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class good_array {
+
+        public boolean isGoodArray(int[] nums) {
+            int g = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+                g = gcd(g, nums[i]);
+            }
+            return g == 1;
+        }
+
+        private int gcd(int a, int b) {
+            while (b != 0) {
+                int t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        }
+
+}
